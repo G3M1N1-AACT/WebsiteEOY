f7ee7d89dfece6dae51bc2113abfdfef5484097e
G3M1N1-AACT <149721206+G3M1N1-AACT@users.noreply.github.com>
2024-03-11 17:03:17 -0300

    up

---
+++ JAVASCRIPT/Characters.js
@@ -0,0 +1,5 @@
+var audio = new Audio("C:\\Users\\daw10\\OneDrive\\Documentos\\PROG1185 - JAVA - DImitri\\Login_Maryssa\\WebsiteEOY\\SOUNDS\\Characters Page.mp3");
+audio.play();
+
+const body = document.body;
+
--- JAVASCRIPT/java.js
+++ JAVASCRIPT/java.js
@@ -1,13 +1,25 @@
-var audio = document.getElementById("myAudio");
 
-// Function to play the audio on repeat
-function playMusicOnRepeat() {
-  audio.play();  // Start playing the audio
-  audio.addEventListener("ended", function() {
-    audio.currentTime = 0;  // Rewind to the beginning when it ends
-    audio.play();  // Start playing again
-  });
+var audio = new Audio("C:\\Users\\daw10\\OneDrive\\Documentos\\PROG1185 - JAVA - DImitri\\Login_Maryssa\\WebsiteEOY\\SOUNDS\\AIW.mp3");
+audio.play();
+
+const body = document.body;
+
+const backgroundImages = [
+  "url(../IMAGES/4.jpg)",
+  "url(../IMAGES/2.jpg)",
+  "url(../IMAGES/cheshire-grin.jpg)",
+  "url(../IMAGES/5.jpg)",
+  "url(../IMAGES/6.jpg)",
+];
+
+function changeBackground(index) {
+  body.style.backgroundImage = backgroundImages[index];
 }
 
-// Call the playMusicOnRepeat function when the page loads (or after the element is loaded)
-window.onload = playMusicOnRepeat;
+const backgroundButtons = document.querySelectorAll(".background-button");
+
+backgroundButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    changeBackground(index);
+  });
+});
